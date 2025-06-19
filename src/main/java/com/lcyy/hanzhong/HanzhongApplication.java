f8297cd6bf88883fb8ca1fed95f2de624bd432ed
dlwlrma <2281874167@qq.com>
2025-06-19 20:41:16 +0800

    第一次提交，完成项目的pom.xml配置

---
+++ src/main/java/com/lcyy/hanzhong/HanzhongApplication.java
@@ -0,0 +1,13 @@
+package com.lcyy.hanzhong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanzhongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanzhongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lcyy/hanzhong/HanzhongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcyy.hanzhong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanzhongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
