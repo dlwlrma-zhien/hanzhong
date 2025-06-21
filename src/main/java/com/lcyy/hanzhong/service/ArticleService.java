4371669869a5eeb0442d87873386ea0e1194cd2d
dlwlrma <2281874167@qq.com>
2025-06-21 15:58:59 +0800

    启动报错，增加了注解

--- src/main/java/com/lcyy/hanzhong/service/ArticleService.java
+++ src/main/java/com/lcyy/hanzhong/service/ArticleService.java
@@ -2,11 +2,13 @@ package com.lcyy.hanzhong.service;
 
 import com.lcyy.hanzhong.entity.Article;
 import com.lcyy.hanzhong.service.base.BaseService;
+import org.springframework.stereotype.Service;
 
 /**
  * @author: dlwlrma
  * @data 2025年06月21日 15:28
  * @Description
  */
+@Service
 public class ArticleService extends BaseService<Article> {
 }
